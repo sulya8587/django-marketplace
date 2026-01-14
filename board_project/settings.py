767fafb5faffd6f1a47037025318d07d624a3190
sulya8587 <sulya8587@gmail.com>
2026-01-14 14:27:12 +0500

    Fix settings.py

--- board_project/settings.py
+++ board_project/settings.py
@@ -75,7 +75,7 @@ ROOT_URLCONF = "board_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],  # you may add BASE_DIR / "templates"
+        "DIRS": [BASE_DIR / "templates"],  
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
